e8569b362af8f919d34621f9a299105ca41ea85e
백재혁 <bjh010321@naver.com>
2024-12-15 00:27:46 +0900

    READ ME 파일 수정

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import Parser from "rss-parser";
  * README.MD에 작성될 페이지 텍스트
  * @type {string}
  */
-let text = `# Jaehyuk Profile👋
+let text = `<h2 align="center"> Jaehyuk Profile👋 </h2> 
 
-## 🔧 Tech Stack
+<h3 align="center"> 🔧 Tech Stack </h3> 
 
-<p>
+<p align="center">
   <img alt="" src= "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"/> 
   <img alt="" src= "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"/> 
   <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white"/> 
@@ -20,7 +20,6 @@ let text = `# Jaehyuk Profile👋
   <img alt="" src= "https://img.shields.io/badge/MySQL-4B89DC?style=for-the-badge&logo=mysql&logoColor=white"/>
   <img alt="" src= "https://img.shields.io/badge/redis-%23DD0031.svg?&style=for-the-badge&logo=redis&logoColor=white"/>
   <img alt="" src= "https://img.shields.io/badge/Amazon_AWS-232F3E?style=for-the-badge&logo=amazon-aws&logoColor=white"/>
-
 </p>
 
 ## 📕 Latest Blog Posts 
