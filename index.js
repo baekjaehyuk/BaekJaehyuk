a91a49823d0ee18a4a85beaf0d1fcb1d045962b2
baekjaehyuk <bjh010321@naver.com>
2025-02-16 01:58:30 +0900

    Update README.md

--- index.js
+++ index.js
@@ -2,24 +2,11 @@ import { writeFileSync } from 'node:fs';
 import Parser from "rss-parser";
 
 
-let text = `<h2> My Profile 👋 </h2> 
+let text = `<h2> 백재혁 | Jaehyuk Baek 👋 </h2> 
 
-- 📘 Myongji UNIV. Convergence Software 
-- 🌱 I’m currently learning BackEnd 
+- 저는 명지대학교 융합소프트웨어학부에 재학 중입니다.
 
-<h3 align="center"> 🔧 Main Stack  </h3> 
-
-<p align="center">
-  <img alt="" src= "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white"/> 
-  <img alt="" src="https://img.shields.io/badge/spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white">
-  <img alt="" src="https://img.shields.io/badge/springboot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white">
-  </br>
-  <img alt="" src= "https://img.shields.io/badge/MySQL-4B89DC?style=for-the-badge&logo=mysql&logoColor=white"/>
-  <img alt="" src= "https://img.shields.io/badge/redis-%23DD0031.svg?&style=for-the-badge&logo=redis&logoColor=white"/>
-  <img alt="" src= "https://img.shields.io/badge/Amazon_AWS-232F3E?style=for-the-badge&logo=amazon-aws&logoColor=white"/>
-</p>
-
-## 📕 Latest 5 Blog Posts
+## 📕 최근 블로그 포스팅
 
 `;
 
